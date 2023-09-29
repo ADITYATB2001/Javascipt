db2d4a99c504352bcdbf37369a21c1044dc07163
ADITYA T B <adityatb2001@gmail.com>
2023-09-29 05:33:29 +0000

    datatypes

---
+++ .devcontainer/Basics/datatypes.js
@@ -0,0 +1,9 @@
+let name = "Aditya"
+let age = 21
+let time;
+let temp = null
+
+//null and undefined are different. null is an object
+
+//console.log(typeof temp);
+console.table([typeof age,typeof time])
