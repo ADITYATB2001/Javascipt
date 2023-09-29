ac1c420f43e0d940dd4a3f9771bb5b23f5869aca
ADITYA T B <adityatb2001@gmail.com>
2023-09-29 05:46:10 +0000

    conversion of dataType

---
+++ .devcontainer/Basics/conversion.js
@@ -0,0 +1,11 @@
+let score = undefined
+let value = Number(score)
+// console.log(typeof value)
+// console.log(value);
+
+// string => number; null => 0; true => 1;undefined = NaN
+
+let isLogged = 1
+let logged = Boolean(isLogged)
+console.log(typeof logged)
+console.log(logged)
