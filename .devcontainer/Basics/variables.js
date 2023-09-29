8d3ac7116e5c8c509649d70d163b9915de6c3095
ADITYA T B <adityatb2001@gmail.com>
2023-09-29 05:29:48 +0000

    variables

---
+++ .devcontainer/Basics/variables.js
@@ -0,0 +1,8 @@
+const accountID = 11323
+let accountName = "Aditya"
+var accountPass = "1233"
+
+console.table([accountID,accountName,accountPass]);
+/*
+prefer not to use var because they don't allow scope
+*/
